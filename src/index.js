c517bc363ae5eca679ba8d70dcbed918c4d90d0c
Kosukekim <tight1112@yahoo.co.jp>
2023-05-07 04:41:28 +0900

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 // import App from './App';
 import Page1 from "./pages/page1/page1"
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Page1 />
+    <App />
   </React.StrictMode>
 );
 
