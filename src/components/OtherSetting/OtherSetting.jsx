16c081a148bf382c6903a0bd63dd2c2e5895a1b6
Kosukekim <tight1112@yahoo.co.jp>
2023-05-07 20:28:39 +0900

    made each type of setting have an input element and it is also saved to sessionStrage of the browser. so if ones switch the page(and also url), the inputted items are preserved.

--- src/components/OtherSetting/OtherSetting.jsx
+++ src/components/OtherSetting/OtherSetting.jsx
@@ -1,9 +1,17 @@
 import "./OtherSetting.css"
 
+var storage = sessionStorage;
 export default function OtherSetting() {
+  function handleOtherStg(event){
+    console.log(event.target.value);
+    var k = "OtherStg";
+    var v = event.target.value;
+    storage.setItem(k, v);
+  }
   return (
     <div className='otherStg'>
       <p>Other Setting</p>
+      <input type="text" placeholder="othersetting" defaultValue={storage.getItem("OtherStg")} onChange={(event) => handleOtherStg(event)}></input>
     </div>
   )
 }
